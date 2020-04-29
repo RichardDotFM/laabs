81c306274b61917e7dbd8918f198d4405b4e0ef9
marusa228 <64519179+marusa228@users.noreply.github.com>
2020-04-29 22:38:02 +0300

    Update Palindrome.java

--- lab1/src/Palindrome.java
+++ lab1/src/Palindrome.java
@@ -1,20 +1,31 @@
 public class Palindrome {
 
-    public static void main(String[] args) {
-        for (int j = 0; j < args.length; j++) {
-            String s = args[j];
-            System.out.println("Слово " + s + " палиндром? " + isPalindrome(s));
-        }
-    }
-    public static boolean isPalindrome(String s) {
-        char[] str = s.toCharArray();
-        for (int j = 0; j < str.length/2; j++) {
-            if (str[j] != str[str.length - 1 - j]) {
+    static boolean isPalindrome(String str)
+    {
+
+        //Указатели, указывающие на начало
+        // и конец строки
+        int i = 0, j = str.length() - 1;
+
+        // Пока есть символы для сравнения
+        while (i < j) {
+
+            // если есть несоответствие
+            if (str.charAt(i) != str.charAt(j))
                 return false;
-            }
+            i++;
+            j--;
         }
         return true;
-
     }
 
+    public static void main(String[] args)
+    {
+        String str = "шалаш";
+
+        if (isPalindrome(str))
+            System.out.print("Да");
+        else
+            System.out.print("Нет");
+    }
 }
